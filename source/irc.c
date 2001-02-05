03e331a916991f9ded308b1df98ffc8d451cf817
jnelson <jnelson>
2001-02-05 16:52:21 +0000

    Commit #35 -- Update the KNOWNBUGS file since 0.9.15 Update the UDPATES file since last september (sheesh)

--- source/irc.c
+++ source/irc.c
@@ -25,7 +25,7 @@ const char internal_version[] = "20010202";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 34;
+const unsigned long	commit_id = 35;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
