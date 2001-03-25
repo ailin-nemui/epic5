8aa893aa8fd760828cd88a6317b1c3b99509b165
jnelson <jnelson>
2001-03-25 05:08:46 +0000

    /on topic throws three arguments, not two; fix the numargs value for /on topic in hook.c

--- source/irc.c
+++ source/irc.c
@@ -13,19 +13,19 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC4-0.10.1";
-const char useful_info[] = "epic4 0 10 1";
+const char irc_version[] = "EPIC4-0.10.2";
+const char useful_info[] = "epic4 0 10 2";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20010323";
+const char internal_version[] = "20010324";
 
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 65;
+const unsigned long	commit_id = 66;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
