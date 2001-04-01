b1868cee1098a5815133527032a8da79990f7423
jnelson <jnelson>
2001-04-01 04:17:24 +0000

    EPIC4-1.0, the real thing this time!

--- source/irc.c
+++ source/irc.c
@@ -13,19 +13,19 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC4-0.10.4";
-const char useful_info[] = "epic4 0 10 4";
+const char irc_version[] = "EPIC4-1.0";
+const char useful_info[] = "epic4 1 0";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20010329";
+const char internal_version[] = "20010401";
 
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 69;
+const unsigned long	commit_id = 70;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
