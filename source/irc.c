f01394609e39d481d5954ea5e01b10b0601f4de1
robohak <robohak>
2001-04-13 08:04:10 +0000

    update commit_id for the fixes to server.c and names.c where a disconnected server wasn't being handled correctly

--- source/irc.c
+++ source/irc.c
@@ -25,7 +25,7 @@ const char internal_version[] = "20010401";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 72;
+const unsigned long	commit_id = 73;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
