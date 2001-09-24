0d667d1867a2d007df030a1196f2c6eaa0aaa08f
jnelson <jnelson>
2001-09-24 18:44:04 +0000

    Update the commit id that crazyeddy didn't update. ;-)

--- source/irc.c
+++ source/irc.c
@@ -25,7 +25,7 @@ const char internal_version[] = "20010730";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 80;
+const unsigned long	commit_id = 81;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
