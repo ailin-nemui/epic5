f9833a2d44f0e1955a9b2d4a5725df243b5ad0a4
crazyed <crazyed>
2001-10-10 23:33:36 +0000

    Bumped commit number.  :-)

--- source/irc.c
+++ source/irc.c
@@ -25,7 +25,7 @@ const char internal_version[] = "20010730";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 98;
+const unsigned long	commit_id = 99;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
