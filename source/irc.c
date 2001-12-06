e95da41a37a7ed6dc180af66a82a70fdb0f9ab1a
jnelson <jnelson>
2001-12-06 16:56:09 +0000

    Bump commit_id after crazyeddy's last commit.

--- source/irc.c
+++ source/irc.c
@@ -25,7 +25,7 @@ const char internal_version[] = "20011203";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 168;
+const unsigned long	commit_id = 169;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
