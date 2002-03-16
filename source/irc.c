069cb2e560953094ce54ce69ca9bac184250f8dc
jnelson <jnelson>
2002-03-16 17:44:14 +0000

    Ah screw it.  I'll just release 1.1.3.  What's the harm?

--- source/irc.c
+++ source/irc.c
@@ -13,19 +13,19 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC4-1.1.2";
-const char useful_info[] = "epic4 1 1 2";
+const char irc_version[] = "EPIC4-1.1.3";
+const char useful_info[] = "epic4 1 1 3";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20020220";
+const char internal_version[] = "20020317";
 
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 224;
+const unsigned long	commit_id = 225;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
@@ -33,7 +33,7 @@ const unsigned long	commit_id = 224;
  * reality, I have decided to start doing that with EPIC.  These names
  * are intentionally and maliciously silly.  Complaints will be ignored.
  */
-const char ridiculous_version_name[] = "Palindrome";
+const char ridiculous_version_name[] = "Statutory";
 
 #define __need_putchar_x__
 #include "status.h"
