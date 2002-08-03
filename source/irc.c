db74e28337a1a3f13da58e85252eedea55d18c1d
crazyed <crazyed>
2002-08-03 11:04:05 +0000

    Blergh..  Forgot to increment commit_id.
    For reference, the notes from the last patch:
    
    * Applied MaXxX's patch which readjusts the mirc to ansi colour
      translations.  In particular, the colours for ^c5 and ^c7 have been
      swapped, and the bold/flash values have also been tuned.
    * Re-integrated support for /set term_does_bright_blink.  If this is on,
      bright backgrounds which are not normally supported by ansi terminals
      will be given the blink attribute instead, which many/most xterms
      normally translate to bold.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.325 2002/08/02 16:21:17 crazyed Exp $ */
+/* $EPIC: irc.c,v 1.326 2002/08/03 11:04:05 crazyed Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20020521";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 330;
+const unsigned long	commit_id = 331;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
