eb96354a7326ba1884ab1518cd4747834ca36879
crazyed <crazyed>
2003-02-02 07:57:37 +0000

    * Configure now does a deeper search for the perl libs. * Rebuilt configure.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.441 2003/02/02 07:54:30 crazyed Exp $ */
+/* $EPIC: irc.c,v 1.442 2003/02/02 07:57:37 crazyed Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20021226";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 444;
+const unsigned long	commit_id = 445;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
