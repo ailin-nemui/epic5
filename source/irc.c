1266771c319e2c9348fcfcf9d22dd0c0d4148b58
jnelson <jnelson>
2003-02-04 02:53:53 +0000

    Really commit 'suppressmotd' this time.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.448 2003/02/04 02:53:11 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.449 2003/02/04 02:53:53 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20021226";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 452;
+const unsigned long	commit_id = 453;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
