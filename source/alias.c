177408b3e5f98a3894a404356a90d8e544cf30a9
wd <wd>
2003-02-27 20:20:56 +0000

    Fix a little bug in /stack (alias|assign) pop. -wd

--- source/alias.c
+++ source/alias.c
@@ -1,4 +1,4 @@
-/* $EPIC: alias.c,v 1.15 2003/02/17 23:48:48 crazyed Exp $ */
+/* $EPIC: alias.c,v 1.16 2003/02/27 20:20:56 wd Exp $ */
 /*
  * alias.c -- Handles the whole kit and caboodle for aliases.
  *
@@ -2587,7 +2587,10 @@ void	do_stack_alias (int type, char *args, int which)
 					prev->next = aptr->next;
 
 				/* throw away anything we already have */
-				delete_cmd_alias(args, 0);
+				if (which == STACK_DO_ALIAS)
+					delete_cmd_alias(args, 0);
+				else
+					delete_var_alias(args, 0);
 
 				/* put the new one in. */
 				if (aptr->list)
--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.467 2003/02/27 02:59:25 crazyed Exp $ */
+/* $EPIC: irc.c,v 1.468 2003/02/27 20:20:56 wd Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20021226";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 471;
+const unsigned long	commit_id = 472;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
