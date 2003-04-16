a2bd53e7e156f1cd23aec67654c1384074aaaf0c
crazyed <crazyed>
2003-04-16 07:22:03 +0000

    * Remembered to increment commit_id. * Removed certain local dependancies from the last commit.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.498 2003/04/10 23:14:56 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.499 2003/04/16 07:22:03 crazyed Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20030326";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 502;
+const unsigned long	commit_id = 503;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
