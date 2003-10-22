ba4e46ca08686038b235c7bebddfde48002d9751
jnelson <jnelson>
2003-10-22 16:05:10 +0000

    Ok.  Maybe my previous fix was wrong.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.602 2003/10/22 15:50:23 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.603 2003/10/22 16:05:10 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20031019";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 607;
+const unsigned long	commit_id = 608;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
