f2b043e378f25bd7a2872aa03355bbd1c0d47eb0
jnelson <jnelson>
2003-12-07 22:04:22 +0000

    Update the epic man page to include -o and -O EPIC-1.2.4!

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.658 2003/12/07 21:26:29 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.659 2003/12/07 22:04:25 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -40,19 +40,19 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC4-1.2.3";
-const char useful_info[] = "epic4 1 2 3";
+const char irc_version[] = "EPIC4-1.2.4";
+const char useful_info[] = "epic4 1 2 4";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20031205";
+const char internal_version[] = "20031207";
 
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 663;
+const unsigned long	commit_id = 664;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
@@ -60,7 +60,7 @@ const unsigned long	commit_id = 663;
  * reality, I have decided to start doing that with EPIC.  These names
  * are intentionally and maliciously silly.  Complaints will be ignored.
  */
-const char ridiculous_version_name[] = "Petulance";
+const char ridiculous_version_name[] = "Preparator";
 
 #define __need_putchar_x__
 #include "status.h"
