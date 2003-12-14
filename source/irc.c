c8f42598e13e8e01204c896c7dc60bc4f4d0d2ee
jnelson <jnelson>
2003-12-14 01:15:14 +0000

    Change default of /SET AUTO_REJOIN to OFF.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.665 2003/12/13 17:39:52 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.666 2003/12/14 01:15:15 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20031207";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 671;
+const unsigned long	commit_id = 672;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
