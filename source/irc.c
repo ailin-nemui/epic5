0a87123a1dc100c9df3b48850e4b635c9ce9d4fc
crazyed <crazyed>
2004-09-10 04:01:48 +0000

    *cough*

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.784 2004/09/10 03:43:49 crazyed Exp $ */
+/* $EPIC: irc.c,v 1.785 2004/09/10 04:01:48 crazyed Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20040319";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1115;
+const unsigned long	commit_id = 1116;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
@@ -645,7 +645,6 @@ static	void	parse_args (int argc, char **argv)
 	 */
 	while ((ch = getopt(argc, argv, "aBbc:dfFhH:l:L:n:oOp:qsvxz:")) != EOF)
 	{
-fprintf(stderr,"SHIT: %c %d/%d %d %c %s\n", ch, optind, argc, opterr, optopt, optarg);
 		switch (ch)
 		{
 			case 'v':	/* Output ircII version */
