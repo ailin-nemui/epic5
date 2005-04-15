839ade9847961cf42755253f45b6398e7c7c67c0
jnelson <jnelson>
2005-04-15 02:55:00 +0000

    I botched the commit of the 'builtins' script.  That's about par for the course.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.879 2005/04/15 02:47:45 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.880 2005/04/15 02:55:04 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20050316";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1208;
+const unsigned long	commit_id = 1209;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
