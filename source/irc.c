d69643c35d839a1ec47402a2a323bf0b215a3bf9
jnelson <jnelson>
2005-04-20 03:21:13 +0000

    One more round of altchan.bj, and a bunch of final documentation.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.884 2005/04/19 13:43:33 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.885 2005/04/20 03:21:15 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20050316";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1213;
+const unsigned long	commit_id = 1214;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
