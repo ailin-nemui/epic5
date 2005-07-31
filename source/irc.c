80a8512fc38c328d2b0fb38659e5eee0235366dc
jnelson <jnelson>
2005-07-31 03:22:27 +0000

    Fix tabkey.jm with some pf problems.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.937 2005/07/30 20:00:34 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.938 2005/07/31 03:22:31 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20050420";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1265;
+const unsigned long	commit_id = 1266;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
