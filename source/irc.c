7e1291b36451cba42e65b95684f6093dc56f1356
jnelson <jnelson>
2005-08-02 03:36:23 +0000

    More changes from jm.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.938 2005/07/31 03:22:31 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.939 2005/08/02 03:36:25 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20050420";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1266;
+const unsigned long	commit_id = 1267;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
