67afc7bccbef352a85af01f9eb65c60fd78f6542
jnelson <jnelson>
2005-08-03 04:47:21 +0000

    "altchan.bj" was renamed to 'altchan" at black's request.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.941 2005/08/03 04:21:39 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.942 2005/08/03 04:47:22 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20050420";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1269;
+const unsigned long	commit_id = 1270;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
