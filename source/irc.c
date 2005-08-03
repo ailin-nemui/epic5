3a6f10b57610aad9f5fb355f847b1df1aa016889
jnelson <jnelson>
2005-08-03 05:06:26 +0000

    Ooops. forgot to update commit id.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.942 2005/08/03 04:47:22 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.943 2005/08/03 05:06:26 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20050420";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1270;
+const unsigned long	commit_id = 1271;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
