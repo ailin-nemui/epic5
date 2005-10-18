257199e95d4251067a8bd7cf96a51a615e655ea3
jnelson <jnelson>
2005-10-18 01:14:01 +0000

    Roll back new expr's alias_special_char() so it permits the slurping of function calls, so @ foo.bar.$encode(moo) = 5   works again. sigh.

--- source/expr2.c
+++ source/expr2.c
@@ -1,4 +1,4 @@
-/* $EPIC: expr2.c,v 1.31 2005/10/16 15:46:01 jnelson Exp $ */
+/* $EPIC: expr2.c,v 1.32 2005/10/18 01:14:01 jnelson Exp $ */
 /*
  * Zsh: math.c,v 3.1.2.1 1997/06/01 06:13:15 hzoli Exp 
  * math.c - mathematical expression evaluation
@@ -2580,7 +2580,7 @@ static  char *	after_expando_special (expr_info *c)
 
 	for (;;)
 	{
-		rest = after_expando(start, 1, &call);
+		rest = after_expando(start, 0, &call);
 		if (*rest != '$')
 			break;
 		start = rest + 1;
