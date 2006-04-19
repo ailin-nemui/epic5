74c4c46d86d0556434f3e6242a4bdc1647066858
jnelson <jnelson>
2006-04-19 01:21:37 +0000

    Add 'channelmonitor' script from larne.  Thanks!

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1013 2006/03/05 23:03:17 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1014 2006/04/19 01:21:39 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20060303";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1341;
+const unsigned long	commit_id = 1342;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
