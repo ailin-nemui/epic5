da379953078a51eef09527c14239966657fbf678
jnelson <jnelson>
2006-08-27 21:06:23 +0000

    Add /set auto_reconnect_delay, thanks nullie! =)

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1059 2006/08/27 20:12:04 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1060 2006/08/27 21:06:27 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20060715";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1387;
+const unsigned long	commit_id = 1389;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
