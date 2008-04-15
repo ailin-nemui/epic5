3c450f19857cae005b8e23438e60771c7560080c
jnelson <jnelson>
2008-04-15 04:49:49 +0000

    Fix signal handling for SIG_IGN and SIG_DFL so SIGPIPE doesn't cause crash.

--- source/ircsig.c
+++ source/ircsig.c
@@ -1,4 +1,4 @@
-/* $EPIC: ircsig.c,v 1.7 2008/04/04 04:51:05 jnelson Exp $ */
+/* $EPIC: ircsig.c,v 1.8 2008/04/15 04:49:51 jnelson Exp $ */
 /*
  * ircsig.c: has a `my_signal()' that uses sigaction().
  *
@@ -71,61 +71,74 @@ static RETSIGTYPE signal_handler (int sig_no)
 		signal_handlers[sig_no](sig_no);
 }
 
+sigfunc *reset_one_signal (int sig_no, sigfunc *sig_handler)
+{
+	struct sigaction sa, osa;
+
+	if (sig_no < 0)
+		return NULL;			/* Signal not implemented */
+
+	signal_handlers[sig_no] = NULL;
+
+	sa.sa_handler = sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, sig_no);
+
+	/* this is ugly, but the `correct' way.  i hate c. -mrg */
+	sa.sa_flags = 0;
+#if defined(SA_RESTART) || defined(SA_INTERRUPT)
+	if (SIGALRM == sig_no || SIGINT == sig_no)
+	{
+# if defined(SA_INTERRUPT)
+		sa.sa_flags |= SA_INTERRUPT;
+# endif /* SA_INTERRUPT */
+	}
+	else
+	{
+# if defined(SA_RESTART)
+		sa.sa_flags |= SA_RESTART;
+# endif /* SA_RESTART */
+	}
+#endif /* SA_RESTART || SA_INTERRUPT */
+
+	if (0 > sigaction(sig_no, &sa, &osa))
+		return SIG_ERR;
+
+	return osa.sa_handler;
+}
+
+
 /* hook_all_signals needs to be called in main() before my_signal()
  * if any signal hooks fail, it returns SIG_ERR, otherwise it returns
  * NULL. - pegasus
  */
-sigfunc *init_signals (void)
+sigfunc *	init_signals (void)
 {
-	struct sigaction sa, osa;
 	int sig_no;
 	sigfunc *error = NULL;
 
 	memset(&signals_caught, 0, NSIG * sizeof(int));
-	sa.sa_handler = &signal_handler;
 
 	for (sig_no = 0; sig_no < NSIG; sig_no++)
 	{
-		signal_handlers[sig_no] = NULL;
-		/* this is ugly, but the `correct' way.  i hate c. -mrg */
-		/* moved from my_signal. -pegasus */
-		sa.sa_flags = 0;
-#if defined(SA_RESTART) || defined(SA_INTERRUPT)
-		if (SIGALRM == sig_no || SIGINT == sig_no)
-		{
-# if defined(SA_INTERRUPT)
-			sa.sa_flags |= SA_INTERRUPT;
-# endif /* SA_INTERRUPT */
-		}
-		else
-		{
-# if defined(SA_RESTART)
-			sa.sa_flags |= SA_RESTART;
-# endif /* SA_RESTART */
-		}
-#endif /* SA_RESTART || SA_INTERRUPT */
-		/* if it wasn't for the above code, we could move the
-		 * sigemptyset() and sigaction() calls outside the loop
-		 * proper. -pegasus
-		 */
-		sigemptyset(&sa.sa_mask);
-		sigaddset(&sa.sa_mask, sig_no);
-		if (0 > sigaction(sig_no, &sa, &osa))
+		if ((reset_one_signal(sig_no, signal_handler)) == SIG_ERR)
 			error = SIG_ERR;
 	}
 	return error;
 }
 
-sigfunc *my_signal (int sig_no, sigfunc *sig_handler)
+sigfunc *	my_signal (int sig_no, sigfunc *sig_handler)
 {
 	sigfunc	*old;
 
-	if (sig_no < 0)
-		return NULL;	/* Signal not implemented */
-
-	/* Well this is certainly simpler. - pegasus */
 	old = signal_handlers[sig_no];
-	signal_handlers[sig_no] = (sigfunc *)sig_handler;
+	if (sig_handler == SIG_IGN || sig_handler == SIG_DFL)
+		reset_one_signal(sig_no, sig_handler);
+	else
+	{
+		reset_one_signal(sig_no, signal_handler);
+		signal_handlers[sig_no] = (sigfunc *)sig_handler;
+	}
 
 	return old;
 }
