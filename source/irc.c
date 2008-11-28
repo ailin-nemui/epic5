50f531be055a91f927d624ad1a284fe02b1f504b
jnelson <jnelson>
2008-11-28 16:29:48 +0000

    Oops.  False alarm -- the KNOWNBUGS list isn't empty yet.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1239 2008/11/28 16:28:03 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1240 2008/11/28 16:29:50 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -40,8 +40,8 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC5-0.9.0";
-const char useful_info[] = "epic5 0 9 0";
+const char irc_version[] = "EPIC5-0.3.9";
+const char useful_info[] = "epic5 0 3 9";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
@@ -52,7 +52,7 @@ const char internal_version[] = "20081128";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1569;
+const unsigned long	commit_id = 1570;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
@@ -60,7 +60,7 @@ const unsigned long	commit_id = 1569;
  * reality, I have decided to start doing that with EPIC.  These names
  * are intentionally and maliciously silly.  Complaints will be ignored.
  */
-const char ridiculous_version_name[] = "Hyperpyrexia";
+const char ridiculous_version_name[] = "Contumacy";
 
 #define __need_putchar_x__
 #include "status.h"
