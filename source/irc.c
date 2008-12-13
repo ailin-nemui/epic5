e28cc1df2c6c91b2f65e16772233341034cf88d6
jnelson <jnelson>
2008-12-13 02:33:04 +0000

    * Have configure check whether perl/tcl/ruby actually work the way we use them * This should prevent compile errors if the apis change in the future.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1246 2008/12/12 03:44:01 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1247 2008/12/13 02:33:06 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20081130";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1576;
+const unsigned long	commit_id = 1577;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
