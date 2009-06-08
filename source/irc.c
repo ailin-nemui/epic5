38bfcd941e128407de3a5a71afd641a62563059d
jnelson <jnelson>
2009-06-08 06:06:01 +0000

    info.c.sh should /not/ be in the cvs repository.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1262 2009/06/08 00:38:42 howl Exp $ */
+/* $EPIC: irc.c,v 1.1263 2009/06/08 06:06:01 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20081225";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1591;
+const unsigned long	commit_id = 1592;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
