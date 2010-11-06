edc0ad251daedc7e985696624d9cba11333e80c7
jnelson <jnelson>
2010-11-06 02:50:28 +0000

    Fix the logman script after prodding from mel-nix

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1312 2010/06/27 03:14:17 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1313 2010/11/06 02:50:28 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20100430";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1642;
+const unsigned long	commit_id = 1643;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
