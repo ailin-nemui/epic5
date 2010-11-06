3e8bb4be51dc75300cf987e48c0e07a3b29ef63f
jnelson <jnelson>
2010-11-06 16:43:51 +0000

    Argh, more typos.

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1313 2010/11/06 02:50:28 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1314 2010/11/06 16:43:53 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20100430";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1643;
+const unsigned long	commit_id = 1644;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
