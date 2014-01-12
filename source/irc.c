4dcc49d4ed7e63f566df9db4ff0373bf6326b1a5
jnelson <jnelson>
2014-01-12 01:05:08 +0000

    * The sender can be omitted (such as with "NOTICE AUTH"), so don't check those

--- source/irc.c
+++ source/irc.c
@@ -1,4 +1,4 @@
-/* $EPIC: irc.c,v 1.1369 2014/01/10 13:30:41 jnelson Exp $ */
+/* $EPIC: irc.c,v 1.1370 2014/01/12 01:05:11 jnelson Exp $ */
 /*
  * ircII: a new irc client.  I like it.  I hope you will too!
  *
@@ -52,7 +52,7 @@ const char internal_version[] = "20130731";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1700;
+const unsigned long	commit_id = 1701;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
--- source/parse.c
+++ source/parse.c
@@ -1,4 +1,4 @@
-/* $EPIC: parse.c,v 1.97 2014/01/10 13:30:41 jnelson Exp $ */
+/* $EPIC: parse.c,v 1.98 2014/01/12 01:05:11 jnelson Exp $ */
 /*
  * parse.c: handles messages from the server.   Believe it or not.  I
  * certainly wouldn't if I were you. 
@@ -1507,7 +1507,7 @@ void 	parse_server (const char *orig_line, size_t orig_line_size)
 		return;		/* Serious protocol violation -- ByeBye */
 	}
 
-	if (!islegal(*from))
+	if (*from && !islegal(*from))
 	{ 
 		rfc1459_odd(from, comm, ArgList);
 		return;		
