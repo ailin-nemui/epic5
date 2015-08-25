ea4a10fd662d2199389c01e6d30c87e5723c32de
Jeremy Nelson <jnelson@overkill.epicsol.org>
2015-08-25 16:43:20 -0500

    * Be more explicit about how to use the improved automargins feature * Change the default WORD_BREAK to just space and tab.

--- source/irc.c
+++ source/irc.c
@@ -52,7 +52,7 @@ const char internal_version[] = "20150809";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1831;
+const unsigned long	commit_id = 1832;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
