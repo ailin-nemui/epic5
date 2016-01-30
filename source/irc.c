fe00c063c9bf86736f3cb85faa0f78f3f47b16ae
Jeremy Nelson <jnelson@overkill.epicsol.org>
2016-01-29 21:59:39 -0600

    * Fix to make --with-localdir more gentoo friendly (binki)

--- source/irc.c
+++ source/irc.c
@@ -51,7 +51,7 @@ const char internal_version[] = "20160109";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1857;
+const unsigned long	commit_id = 1858;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
