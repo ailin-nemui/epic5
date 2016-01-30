1323fb96f419b437f08f5d2337d34f5f64cc1f5e
Jeremy Nelson <jnelson@overkill.epicsol.org>
2016-01-30 09:26:11 -0600

    * Fix to make parallel builds work better for gentoo (binki)

--- source/irc.c
+++ source/irc.c
@@ -51,7 +51,7 @@ const char internal_version[] = "20160109";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1858;
+const unsigned long	commit_id = 1859;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
