0fbcf477101f5c2abf79eff46473f5505b99f8d5
Jeremy Nelson <jnelson@epicsol.org>
2017-08-08 08:48:07 -0500

    Increment commit id

--- source/irc.c
+++ source/irc.c
@@ -51,7 +51,7 @@ const char internal_version[] = "20160805";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1883;
+const unsigned long	commit_id = 1884;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
