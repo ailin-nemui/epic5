15341b23f1abbe585173988e8d16218493cdf48d
Jeremy Nelson <jnelson@epicsol.org>
2021-03-27 18:13:23 -0500

    I goofed the date - new release.

--- source/irc.c
+++ source/irc.c
@@ -40,19 +40,19 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC5-2.1.3";
-const char useful_info[] = "epic5 2 1 3";
+const char irc_version[] = "EPIC5-2.1.4";
+const char useful_info[] = "epic5 2 1 4";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20220326";
+const char internal_version[] = "20210326";
 
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 1926;
+const unsigned long	commit_id = 1927;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
