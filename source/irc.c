b305f38f4359e81ff0144a7a1c0d99a801e713a3
Jeremy Nelson <jnelson@epicsol.org>
2022-05-09 21:28:28 -0500

    Bump release to 2.1.9, since I botched 2.1.8.

--- source/irc.c
+++ source/irc.c
@@ -40,8 +40,8 @@
 /*
  * irc_version is what $J returns, its the common-name for the version.
  */
-const char irc_version[] = "EPIC5-2.1.7";
-const char useful_info[] = "epic5 2 1 7";
+const char irc_version[] = "EPIC5-2.1.9";
+const char useful_info[] = "epic5 2 1 9";
 
 /*
  * internal_version is what $V returns, its the integer-id for the
@@ -52,7 +52,7 @@ const char internal_version[] = "20220509";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 2052;
+const unsigned long	commit_id = 2053;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
