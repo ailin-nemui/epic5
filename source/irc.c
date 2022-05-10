6fa28184bf93ea38a54a4b795a377a10c7955741
Jeremy Nelson <jnelson@epicsol.org>
2022-05-09 20:44:19 -0500

    New EPIC! (epic5-2.1.8)

--- source/irc.c
+++ source/irc.c
@@ -47,12 +47,12 @@ const char useful_info[] = "epic5 2 1 7";
  * internal_version is what $V returns, its the integer-id for the
  * version, and corresponds to the date of release, YYYYMMDD.
  */ 
-const char internal_version[] = "20220327";
+const char internal_version[] = "20220509";
 
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 2051;
+const unsigned long	commit_id = 2052;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
@@ -60,7 +60,7 @@ const unsigned long	commit_id = 2051;
  * reality, I have decided to start doing that with EPIC.  These names
  * are intentionally and maliciously silly.  Complaints will be ignored.
  */
-const char ridiculous_version_name[] = "Cinereous";
+const char ridiculous_version_name[] = "Decoupage";
 
 #define __need_putchar_x__
 #include "status.h"
